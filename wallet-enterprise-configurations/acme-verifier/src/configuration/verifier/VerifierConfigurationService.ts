4937c9dd472d94f1ff52894c618b15aa3862f921
pstamatop <pstamatop@gmail.com>
2024-11-14 16:51:43 +0200

    [WIP] Configurable presentation definition

--- wallet-enterprise-configurations/acme-verifier/src/configuration/verifier/VerifierConfigurationService.ts
+++ wallet-enterprise-configurations/acme-verifier/src/configuration/verifier/VerifierConfigurationService.ts
@@ -6,7 +6,36 @@ import { VerifierConfigurationInterface } from "../../services/interfaces";
 import "reflect-metadata";
 
 
-const pidDescriptor = {
+const sdJwtPidFields = [
+	{
+		"name": "Credential type",
+		"path": ["$.vct"],
+		"filter": {
+			"type": "string",
+			"enum": [
+				"https://example.bmi.bund.de/credential/pid/1.0",
+				"urn:eu.europa.ec.eudi:pid:1"
+			]
+		}
+	},
+	{
+		"name": "Given Name",
+		"path": ['$.given_name'],
+		"filter": {}
+	},
+	{
+		"name": "Family Name",
+		"path": ['$.family_name'],
+		"filter": {}
+	},
+	{
+		"name": "Birthdate",
+		"path": ['$.birthdate'],
+		"filter": {}
+	}
+]
+
+const sdJwtPidDescriptor = {
 	"id": "VerifiableId",
 	"format": {
 		"vc+sd-jwt": {
@@ -16,88 +45,55 @@ const pidDescriptor = {
 		},
 	},
 	"constraints": {
-		"fields": [
-			{
-				"name": "Credential type",
-				"path": [
-					"$.vct"
-				],
-				"filter": {
-					"type": "string",
-					"enum": [
-						"https://example.bmi.bund.de/credential/pid/1.0",
-						"urn:eu.europa.ec.eudi:pid:1"
-					]
-				}
-			},
-			{
-				"name": "Given Name",
-				"path": ['$.given_name'],
-				"filter": {}
-			},
-			{
-				"name": "Family Name",
-				"path": ['$.family_name'],
-				"filter": {}
-			},
-			{
-				"name": "Birthdate",
-				"path": ['$.birthdate'],
-				"filter": {}
-			}
-		]
+		"fields": sdJwtPidFields
 	}
 }
 
-const mdocPIDPresentationDefinition = {
-	"id": "MdocPID",
-	"title": "MDOC PID",
-	"description": "Required Fields: Given Name, Family Name, Age Over 18, BirthDate",
-	"input_descriptors": [
-		{
-			"id": "eu.europa.ec.eudi.pid.1",
-			"format": {
-				"mso_mdoc": {
-					"alg": [
-						"ES256"
-					]
-				},
-			},
-			"constraints": {
-				"limit_disclosure": "required",
-				"fields": [
-					{
-						"name": "Family Name",
-						"path": [
-							"$['eu.europa.ec.eudi.pid.1']['family_name']"
-						],
-						"intent_to_retain": false
-					},
-					{
-						"name": "Given Name",
-						"path": [
-							"$['eu.europa.ec.eudi.pid.1']['given_name']"
-						],
-						"intent_to_retain": false
-					},
-					{
-						"name": "Birthdate",
-						"path": [
-							"$['eu.europa.ec.eudi.pid.1']['birth_date']"
-						],
-						"intent_to_retain": false
-					},
-					{
-						"name": "Age over 18",
-						"path": [
-							"$['eu.europa.ec.eudi.pid.1']['age_over_18']"
-						],
-						"intent_to_retain": false
-					},
-				]
-			}
-		}
-	]
+
+const mdocPidFields = [
+	{
+		"name": "Family Name",
+		"path": [
+			"$['eu.europa.ec.eudi.pid.1']['family_name']"
+		],
+		"intent_to_retain": false
+	},
+	{
+		"name": "Given Name",
+		"path": [
+			"$['eu.europa.ec.eudi.pid.1']['given_name']"
+		],
+		"intent_to_retain": false
+	},
+	{
+		"name": "Birthdate",
+		"path": [
+			"$['eu.europa.ec.eudi.pid.1']['birth_date']"
+		],
+		"intent_to_retain": false
+	},
+	{
+		"name": "Age over 18",
+		"path": [
+			"$['eu.europa.ec.eudi.pid.1']['age_over_18']"
+		],
+		"intent_to_retain": false
+	},
+]
+
+const mdocPidDescriptor = {
+	"id": "eu.europa.ec.eudi.pid.1",
+	"format": {
+		"mso_mdoc": {
+			"alg": [
+				"ES256"
+			]
+		},
+	},
+	"constraints": {
+		"limit_disclosure": "required",
+		"fields": mdocPidFields
+	}
 }
 
 
@@ -109,14 +105,39 @@ export class VerifierConfigurationService implements VerifierConfigurationInterf
 		return [
 			{
 				"id": "PID",
-				"title": "PID",
+				"title": "SD-JWT PID",
 				"description": "Required Fields: VC type, Given Name, Family Name & Birthdate",
-				"format": { "vc+sd-jwt": { alg: ['ES256'] } },
 				"input_descriptors": [
-					pidDescriptor
+					sdJwtPidDescriptor
 				]
 			},
-			mdocPIDPresentationDefinition,
+			{
+				"id": "MdocPID",
+				"title": "MDOC PID",
+				"description": "Required Fields: Given Name, Family Name, Age Over 18, BirthDate",
+				"input_descriptors": [
+					mdocPidDescriptor
+				]
+			},
+			{
+				"id": "CustomVerifiableId",
+				"title": "Custom Verifiable ID",
+				"description": "Select the format and the fields you want to request.",
+				_selectable: true,
+				"input_descriptors": [
+					{
+						"id": undefined,
+						"format": undefined,
+						"constraints": {
+							"limit_disclosure": "required",
+							"fields": [
+								...sdJwtPidFields,
+								...mdocPidFields
+							]
+						}
+					}
+				]
+			},
 		]
 	}
 
